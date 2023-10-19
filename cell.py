8fcbb7b74d0b51872188fe19eaeb853e40c63d43
krenusz <58508664+krenusz@users.noreply.github.com>
2023-10-19 17:28:25 +0200

    Add files via upload

--- cell.py
+++ cell.py
@@ -1,39 +1,66 @@
+
+
 import ray
-ray.init(ignore_reinit_error=True,address='auto')
-## Possibly set up exact wd
-#ray.init(ignore_reinit_error=True,address='auto', runtime_env={"working_dir": "C:/Users/..."})
+ray.init(ignore_reinit_error=True,address='auto', runtime_env={"working_dir": "C:/Users/User/.conda/envs/mtgat"})
 
 @ray.remote
-def cells1(env, agent, state, i_epoch, cnt):
+def cells1(env, agent, state, i_epoch, cnt, lvl, done):
     from threadpoolctl import threadpool_limits
+    #from environment_dish import Environment
     from collections import namedtuple
 
     Transition = namedtuple('Transition', ['state', 'action',  'a_log_prob', 'reward', 'next_state'])
     
+    
     with threadpool_limits(limits=1, user_api='blas'):
+        score = 0
+        
+        if done:
+            agent.update(i_epoch) # update and clear experience
+            return env, agent, state, done
         
         action, action_prob = agent.select_action(state)
         print('action',action,'state',state)
 
-        next_state, reward, _, _, food1_loc, food2_loc, food3_loc, _, food1_collected, food2_collected, food3_collected = env.step(action,state)
+        next_state, reward_lvl1, reward_lvl2, _, _, food1_loc, food2_loc, food3_loc, _, food1_collected, food2_collected, food3_collected = env.step(action,state)
+        
+        if lvl == 1:
+            reward = reward_lvl1
+        else:
+            reward = reward_lvl2
+        print('REWARD:',reward)
         trans = Transition(state, action, action_prob, reward, next_state)
 
-        if all(next_state == food1_loc) and reward >= 100:
+        #if all(next_state == food1_loc) and reward >= 100:
+        #    score = 1
+        #    print('food1 collected')
+        #elif all(next_state == food2_loc) and reward >= 100:
+        #    score = 1
+        #    print('food2 collected')
+        #elif all(next_state == food3_loc) and reward >= 100:
+        #    score = 1
+        #    print('food3 collected')
+        if reward == 90 and lvl == 1:
             score = 1
-            print('food1 collected')
-        elif all(next_state == food2_loc) and reward >= 100:
-            score = 1
-            print('food2 collected')
-        elif all(next_state == food3_loc) and reward >= 100:
-            score = 1
-            print('food3 collected')
-        else:
+            print('CONDITION: food collected')
+        elif reward == 90 and lvl == 2:
+            done = True
+            
+            print('place taken')
+
+        elif reward != 90 and lvl == 1:
             score = 0
+            print('CONDITION: food not collected')
+        elif reward != 9 and lvl == 2:
+            done = False
         agent.store_transition(trans)
 
-        if cnt == 150: 
+        
+        
+        if len(agent.buffer) >= agent.batch_size:
             print('done: agent getting updated')
-            if len(agent.buffer) >= agent.batch_size:
-                agent.update(i_epoch)
-
-    return env, agent, next_state, score
+            agent.update(i_epoch)
+    if lvl == 1:
+        return env, agent, next_state, score
+    else:
+        return env, agent, next_state, done
--- environment_dish.py
+++ environment_dish.py
@@ -33,6 +33,28 @@ class Environment:
         self.max_episode_length = 150
         self.state_observation = [self.x, self.y]
         self.reward = 0
+        self.lvl_1_reward_table = np.array([ [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 1, 90, 1, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 2, 3, 2, 0],
+                                        [0, 0, 3, 0, 0, 2, 3, 90, 3, 2],
+                                        [0, 3, 90, 3, 0, 0, 2, 3, 2, 0],
+                                        [0, 0, 3, 0, 0, 0, 0, 2, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        
+        
+        self.lvl_2_reward_table = np.array([ [0, 0, 0, 0, 0, 0, 1, 2, 1, 0],
+                                        [0, 0, 0, 0, 0, 1, 2, 3, 2, 1],
+                                        [0, 0, 0, 0, 1, 2, 3, 4, 3, 2],
+                                        [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                        [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                        [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                        [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                        [0, 0, 0, 1, 1, 2, 3, 4, 3, 2],
+                                        [0, 0, 0, 0, 0, 1, 2, 3, 2, 1],
+                                        [0, 0, 0, 0, 0, 0, 1, 2, 1, 0]])
     # Reset the agent at the start of each episode
     def reset(self):
 
@@ -45,11 +67,38 @@ class Environment:
         self.x, self.y = 0, 0
         self.state_observation = [self.x, self.y]
         self.reward = 0
+        self.lvl_1_reward_table = np.array([ [0, 0, 0, 0, 0, 1, 2, 1, 0, 0],
+                                             [0, 0, 0, 0, 1, 2, 90, 2, 1, 0],
+                                             [0, 0, 0, 0, 0, 1, 2, 1, 0, 0],
+                                             [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                             [0, 0, 1, 0, 0, 0, 0, 2, 0, 0],
+                                             [0, 1, 2, 1, 0, 0, 2, 3, 2, 0],
+                                             [1, 2, 3, 2, 1, 2, 3, 90, 3, 2],
+                                             [2, 3, 90, 3, 2, 1, 2, 3, 2, 0],
+                                             [1, 2, 3, 2, 1, 0, 0, 2, 0, 0],
+                                             [0, 1, 2, 1, 0, 0, 0, 0, 0, 0]])
+        
+        
+        self.lvl_2_reward_table = np.array([ [0, 0, 0, 0, 0, 0, 1, 2, 1, 0],
+                                             [0, 0, 0, 0, 0, 1, 2, 3, 2, 1],
+                                             [0, 0, 0, 0, 1, 2, 3, 4, 3, 2],
+                                             [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                             [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                             [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                             [0, 0, 0, 1, 2, 3, 4, 90, 4, 3],
+                                             [0, 0, 0, 1, 1, 2, 3, 4, 3, 2],
+                                             [0, 0, 0, 0, 0, 1, 2, 3, 2, 1],
+                                             [0, 0, 0, 0, 0, 0, 1, 2, 1, 0]])
         return [self.x, self.y]
     
     # Returns the number of actions in the action set
     def action_space(self):
         return self.actions
+    
+    #def reset_reward_map(self):
+        
+        
+
     # Agent takes the step, i.e. take action to interact with 
     #  the environment
     def step(self, action, current_state):
@@ -57,34 +106,73 @@ class Environment:
         #if self.state_observation == [self.MAX_HOR_VAL, self.MAX_VER_VAL]:
         #    self.done = True
         #    return np.array(self.state_observation), self.reward, self.done, self.episode_length
-        self.reward = 0
+        self.reward_lvl1 = 0
+        self.reward_lvl2 = 0
         if self.food1_collected == True and self.food2_collected == True and self.food3_collected == True:
             print('all the food collected')
             self.done = True
             self.all_food_collected = True
-            return np.array(self.state_observation), self.reward, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
+            return np.array(self.state_observation), self.reward_lvl1, self.reward_lvl2, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
         
         elif self.episode_length > self.max_episode_length:
             self.done = True
-            return np.array(self.state_observation), self.reward, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
+            return np.array(self.state_observation), self.reward_lvl1, self.reward_lvl2, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
         
         self.action = action
         self.current_state_observation = current_state
         self.state_observation = self.take_action()
-        self.reward = self.get_reward()
+        self.reward_lvl1, self.reward_lvl2 = self.get_reward()
         self.episode_length += 1
                
-        return np.array(self.state_observation), self.reward, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
+        return np.array(self.state_observation), self.reward_lvl1, self.reward_lvl2, self.done, self.all_food_collected, self.food1_loc, self.food2_loc, self.food3_loc, self.episode_length, self.food1_collected, self.food2_collected, self.food3_collected
     
     def get_reward(self):
+        
+        reward_lvl1 = self.lvl_1_reward_table[self.x, self.y]
+        reward_lvl2 = self.lvl_2_reward_table[self.x, self.y]
 
-        
-        
-        # If agent tries to run out of the grid, penalize -10
+        if all(self.current_state_observation == self.state_observation):
+            reward_lvl1 = -50
+            reward_lvl2 = -50
+
+        if reward_lvl1 == 90 or reward_lvl2 == 90:
+            copy = self.lvl_1_reward_table.copy()
+            copy2 = self.lvl_2_reward_table.copy()
+            
+            copy[self.x, self.y] = -100
+            copy2[self.x, self.y] = -100
+
+            self.lvl_1_reward_table = copy
+            self.lvl_2_reward_table = copy2
+        else:
+            copy = self.lvl_1_reward_table.copy()
+            copy2 = self.lvl_2_reward_table.copy()
+            
+            copy[self.x, self.y] = -50
+            copy2[self.x, self.y] = -50
+
+            self.lvl_1_reward_table = copy
+            self.lvl_2_reward_table = copy2
+
+        return reward_lvl1, reward_lvl2
+    
+    def get_reward_ooo(self):    
+        # If agent tries to run out of the grid, penalize -2
         if all(self.current_state_observation == self.state_observation):
             reward = -10
-   
-        # If agent reaches the food, reward it with 100 
+        
+        # If agent reached Terminal state, reward = 0
+        #elif (self.x, self.y) == (self.MAX_HOR_VAL-1, self.MAX_VER_VAL) and self.action == 2 and (self.food1_collected == True or self.food2_collected == True):
+        #    reward = 5
+        #    print('only 1 food collected')
+        #elif (self.x, self.y) == (self.MAX_HOR_VAL, self.MAX_VER_VAL-1) and self.action == 3 and (self.food1_collected == True or self.food2_collected == True):
+        #    reward = 5
+        #    print('only 1 food collected')
+        # For all other states, penalize agent with -1
+        
+        #if np.sum(self.current_state_observation) >= np.sum(self.state_observation):
+        #   reward += -5
+        # If agent reaches the food, reward it with 5 at the end of the episode
         elif (self.x, self.y) == (self.food1_loc[0], self.food1_loc[1]) and self.food1_collected == False:
             print('food1 collected')
             self.food1_collected = True
@@ -102,8 +190,9 @@ class Environment:
         else:
             reward = -1
 
+        #if self.food1_collected == True and self.food2_collected == True and self.food3_collected == True:
+        #    reward = 200
         return reward
-    
     # Method to take action, remain in the same box if agent tries
     # to run outside the grid, otherwise move one box in the 
     # direction of the action
@@ -130,11 +219,14 @@ class Environment:
                         
         return [self.x, self.y]
     
-    # Method to align the environment between agents
-    def align(self, food1_collected_align, food2_collected_align, food3_collected_align):
-        self.food1_collected = food1_collected_align
-        self.food2_collected = food2_collected_align
-        self.food3_collected = food3_collected_align
-        if self.food1_collected == True and self.food2_collected == True and self.food3_collected == True:
-            self.all_food_collected = True
+    # Method to align the environment
+    def align(self, x, y):
+        
             
+        copy = self.lvl_1_reward_table.copy()
+        copy2 = self.lvl_2_reward_table.copy()
+        copy[x, y] = -1
+        copy2[x, y] = -1
+
+        self.lvl_1_reward_table = copy
+        self.lvl_2_reward_table = copy2
